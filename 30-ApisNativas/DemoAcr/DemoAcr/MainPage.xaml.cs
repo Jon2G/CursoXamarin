a628cd64d04f5ab89f66695b77fdc8bf1b1131ce
Jon2G <jonyjouna@gmail.com>
2022-11-14 01:30:51 -0600

    30

---
+++ 30-ApisNativas/DemoAcr/DemoAcr/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace DemoAcr
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Acr.UserDialogs.UserDialogs.Instance.Alert(TxtName.Text, "Hola desde ACR", "Ok");
+            Acr.UserDialogs.UserDialogs.Instance.Toast("Ok ACR");
+        }
+    }
+}
