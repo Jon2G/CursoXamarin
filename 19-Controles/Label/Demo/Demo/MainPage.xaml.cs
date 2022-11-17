da84dbb1abc15b011b8936b5c59fec7be52bed08
Jon2G <jonyjouna@gmail.com>
2022-11-17 02:02:04 -0600

    19-Label

---
+++ 19-Controles/Label/Demo/Demo/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using NameGenerator.Generators;
+using System;
+using System.ComponentModel;
+using System.Threading;
+using Xamarin.Forms;
+
+namespace Demo
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        public string Texto { get; set; }
+        private RealNameGenerator _generator;
+        public MainPage()
+        {
+            Texto = "Este texto es dinámico";
+            InitializeComponent();
+            _ = new Timer((t) => CambiarTexto(), null, TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(5));
+            _generator = new RealNameGenerator();
+            this.BindingContext = this;
+        }
+
+        private void CambiarTexto()
+        {
+            Texto = _generator.Generate();
+        }
+    }
+}
