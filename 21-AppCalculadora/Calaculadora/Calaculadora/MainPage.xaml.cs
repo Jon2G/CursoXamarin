d07bf4f55e90685102c5f05979d43c7a089f0c7d
Jon2G <jonyjouna@gmail.com>
2022-11-17 00:26:57 -0600

    21

---
+++ 21-AppCalculadora/Calaculadora/Calaculadora/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Calaculadora.ViewModel;
+using Xamarin.Forms;
+
+namespace Calaculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new MainPageViewModel();
+        }
+    }
+}
---
+++ 21-AppCalculadora/Calaculadora/Calaculadora/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,62 @@
+﻿using AngouriMath;
+using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Calaculadora.ViewModel
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Operacion { get; set; }
+
+        public string Resultado { get; set; }
+
+        public ICommand DeleteCommand { get; set; }
+        public ICommand CalcularCommand { get; set; }
+        public MainPageViewModel()
+        {
+            DeleteCommand = new Command(Delete);
+            CalcularCommand = new Command<string>(Calcular);
+        }
+        private void Delete()
+        {
+            Operacion = string.Empty;
+            Resultado = string.Empty;
+        }
+        private void Calcular(string content)
+        {
+            if (content == "=")
+            {
+                Resultado = Calcular().ToString("N2");
+                Operacion = Resultado;
+                return;
+            }
+            else if (content == "!")
+            {
+                Operacion += "*-1";
+                Calcular("=");
+                return;
+            }
+            Operacion += content;
+
+        }
+
+        private float Calcular()
+        {
+            try
+            {
+                Entity expr = Operacion;
+                float resultado = (float)expr.EvalNumerical();
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+                Acr.UserDialogs.UserDialogs.Instance.Alert("Operación no valida.", "Alerta", "Ok");
+                Delete();
+            }
+            return 0;
+        }
+    }
+}
