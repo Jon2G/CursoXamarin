2c8b809af94b3ca35b579bfa3e33faec10f7ceb5
Jon2G <jonyjouna@gmail.com>
2022-11-19 21:21:22 -0600

    11

---
+++ 11-ColeccionesGenericas/Colecciones/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Colecciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listaNumeros = new List<int>(); //Declaración de una lista
+
+            int[] arregloNumeros = new int[] { 3, 6, 8, 10, 50 };
+
+            for (int i = 0; i < 5; i++)
+            {
+                listaNumeros.Add(arregloNumeros[i]); //Agrega los elementos del arreglo a la lista
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(listaNumeros[i]);
+            }
+
+        }
+
+    }
+}
---
+++ 11-ColeccionesGenericas/Diccionarios/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Diccionarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> edades = new Dictionary<string, int>();
+
+            //llenamos el diccionario
+            edades.Add("Juan", 20);
+            edades.Add("Daniel", 29);
+
+            edades["Lalo"] = 31;
+            edades["Antonio"] = 16;
+
+            //Recorrer el diccionario
+
+            foreach (KeyValuePair<string, int> item in edades)
+            {
+                Console.WriteLine("Nombre: {0} Edad: {1} ", item.Key, item.Value);
+            }
+        }
+
+    }
+}
