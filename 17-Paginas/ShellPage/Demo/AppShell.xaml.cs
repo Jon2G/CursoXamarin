61a47ce4f0c9d78dd7bb06568cad9309b8132959
Jon2G <jonyjouna@gmail.com>
2022-11-20 00:49:55 -0600

    17

---
+++ 17-Paginas/ShellPage/Demo/AppShell.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, System.EventArgs e)
+        {
+            await AppShell.Current.Navigation.PopToRootAsync();
+            await AppShell.Current.Navigation.PushAsync(new HelloPage());
+            Shell.Current.FlyoutIsPresented = false;
+        }
+
+        private async void Button_Clicked_1(object sender, System.EventArgs e)
+        {
+            await this.Navigation.PopToRootAsync();
+            await AppShell.Current.Navigation.PushAsync(new AboutPage());
+            Shell.Current.FlyoutIsPresented = false;
+        }
+    }
+}
