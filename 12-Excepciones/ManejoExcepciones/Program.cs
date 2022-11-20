9348ac2472669dd2624d38e787854036172f84aa
Jon2G <jonyjouna@gmail.com>
2022-11-19 21:23:03 -0600

    12

---
+++ 12-Excepciones/ManejoExcepciones/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ManejoExcepciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int numero1 = 9;
+                int numero2 = 0;
+                int resultado = numero1 / numero2;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No es posible dividir un número entre cero: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("El programa ha finalizado");
+            }
+        }
+    }
+}
