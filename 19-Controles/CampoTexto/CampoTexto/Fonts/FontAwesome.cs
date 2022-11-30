2ef73b7802a562e2e4167a21965370bcf4341f3a
Jon2G <jonyjouna@gmail.com>
2022-11-30 15:14:27 -0600

    19

---
+++ 19-Controles/CampoTexto/CampoTexto/Fonts/FontAwesome.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+[assembly: ExportFont("fa-solid-900.ttf", Alias = "FA-SOLID")]
+namespace CampoTexto.Fonts
+{
+    internal class FontAwesome
+    {
+        public const string Eye = "";
+        public const string EyeSlash = "";
+    }
+}
---
+++ 19-Controles/CampoTexto/CampoTexto/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using CampoTexto.Fonts;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+namespace CampoTexto
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            bool mostrado = this.TxtPassword.IsPassword;
+
+            //mostrado = !mostrado;
+            if (mostrado == true)
+            {
+                mostrado = false;
+            }
+            else
+            {
+                mostrado = true;
+            }
+            this.TxtPassword.IsPassword = mostrado;
+        }
+        private async void MostrarOcultarPassword(object sender, EventArgs e)
+        {
+            this.TxtPassword.IsPassword = false;
+            this.BtnVer.Text = FontAwesome.EyeSlash; //Cambiar icono del botón
+            await Task.Delay(1000).
+                ContinueWith(OcultarPassword);
+        }
+        private void OcultarPassword(Task task)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                this.TxtPassword.IsPassword = true;
+                this.BtnVer.Text = FontAwesome.Eye; //Cambiar icono del botón
+            }
+                );
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            string usuario = this.TxtUsuario.Text;
+            string password = this.TxtPassword.Text;
+            if (usuario == "admin" && password == "1234")
+            {
+                //correcto
+                this.DisplayAlert("Correcto", "Bienvenido", "Ok");
+            }
+            else
+            {
+                //limpiar campos de texto 
+                this.TxtUsuario.Text = "";
+                this.TxtPassword.Text = "";
+                this.DisplayAlert("Mensaje de error", "Usuario contraseña incorrectos", "Aceptar");
+            }
+        }
+    }
+}
