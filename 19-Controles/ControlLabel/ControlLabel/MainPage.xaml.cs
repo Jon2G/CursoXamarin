acdb01edb1c5b286709df75fd22f06e934fd97db
Jon2G <jonyjouna@gmail.com>
2022-11-30 15:14:32 -0600

    19

---
+++ 19-Controles/ControlLabel/ControlLabel/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace ControlLabel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            //Cargar el contenido del xaml para representarlo en la vista
+            InitializeComponent();
+        }
+        //Evento clicked del botón 
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            string valor = this.LblDinamico.Text;
+            if (valor == "Texto dinámico")
+            {
+                this.LblDinamico.Text = "Este texto fue modificado";
+            }
+            else
+            {
+                this.LblDinamico.Text = "Texto dinámico";
+            }
+        }
+    }
+}
